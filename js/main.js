2cfead04fc5f934f8a6ad6adfd7f4bbacf7a6519
bandeng77 <102723948+bandeng77@users.noreply.github.com>
2025-11-01 19:40:30 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('AKU SAYANG KAMU DUDUT ANISAH').split('')
+    const titles = ('AKU SAYANG KAMU SALSABILAH ANISAH PUTRI FARIHAH').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
